5c4a8fcf2a6a0343d24f22881f9d4f009d81891f
Connor Gutbrod <139666617+ConnorGutbrod@users.noreply.github.com>
2023-07-16 12:40:52 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gradleton.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
